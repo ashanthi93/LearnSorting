cb8a9c5184401c717b0604cf731df834fd416edd
ashanthi93 <ashanthi1993@gmail.com>
2016-05-17 20:30:46 +0530

    Push project

---
+++ src/sortgame/SortGame.java
@@ -0,0 +1,17 @@
+package sortgame;
+
+/**
+ *
+ * @author ASHI
+ */
+public class SortGame {
+
+    public static HomeScreen homeScreen = new HomeScreen();
+    
+    public static void main(String[] args) {
+        
+        homeScreen.setVisible(true);
+        
+    }
+    
+}
